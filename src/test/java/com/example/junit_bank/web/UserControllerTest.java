a0206dcf244f0b423dc6cc02e491f6b3cdfe2209
eddie <gkehddnjs90@gmail.com>
2023-06-09 08:47:50 +0900

    test

--- src/test/java/com/example/junit_bank/web/UserControllerTest.java
+++ src/test/java/com/example/junit_bank/web/UserControllerTest.java
@@ -84,6 +84,7 @@ public class UserControllerTest extends DummyObject {
         joinReqDto.setEmail("ssar@nate.com");
         joinReqDto.setFullname("하하");
 
+
         String requestBody = om.writeValueAsString(joinReqDto);
         System.out.println("requestBody : " + requestBody);
 
